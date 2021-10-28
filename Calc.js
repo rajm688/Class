c3e91b4d045bd74fa6b77cac2addcfb383e81ef9
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-10-28 15:22:34 +0530

    Add files via upload

---
+++ Calc.js
@@ -0,0 +1,19 @@
+class Calc{
+    constructor(arr){
+        this.arr = arr;
+    }
+getAvg(){
+    if (arr.length == 0){
+        return 0;
+    }
+    else{
+        let a = 0;
+        for(let i=0; i< arr.length;i++){
+            a += arr[i]
+            return a;
+        }
+    }
+}
+}
+let array = new Calc([1,2,3,4,5])
+console.log(a/arr.length)
